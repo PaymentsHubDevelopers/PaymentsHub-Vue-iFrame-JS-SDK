3910abe83cbb0caa14ca00dd9cd749fc19fd8474
PaymentsHubDevelopers <developerporta@paymentshub.com>
2023-06-15 16:54:41 -0400

    css updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: './assets/favicon.svg' }],
+      link: [{ rel: 'icon', type: 'image/svg', href: './assets/favicon.svg' }],
       icon: { source: './assets/favicon.svg' }
     },
   },
